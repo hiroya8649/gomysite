6b6a8e08d16d859a5978135c80c1cd098cacd422
hiroya8649 <happy8649@gmail.com>
2019-08-10 17:42:45 +0800

    resume experience and education part

--- web/static/resume.jsx
+++ web/static/resume.jsx
@@ -7,12 +7,33 @@ export default class Resume extends Component {
       <div className="row">
         <div className="col-sm-12">
           <div className="name">TSUHAO CHANG</div>
+          <div className="birthday">1997-04-09 (22)</div>
           <div className="phone">0987-041-849</div>
           <div className="email">1loya4wk@gmail.com</div>
         </div>
       </div>
       <div className="row">
         <div className="col-sm-8">
+          <div className="title">Profile</div>
+          <div className="desc">My name is Roy, was born and now living in Taiwan. I just finished my studying in National Taiwan University of Science and Technology (台灣科技大學) as a Bachelor. Had lived in Japan for half a year and worked as a part time web engineer when I study in Shibaura Institute of Technology (芝浦工業大学) there as an exchange student. Now I decide to continue my studying from next year. Before that, I would like to find some place to learn about GO, AWS, GCP and other knowledge to improve my skills.</div>
+          <div className="title">Experience</div>
+          <div className="subtitle">Backend & Frontend Developer (Intern)</div>
+          <div className="desc">ACCESS CO. LTD. (in Akihabara)</div>
+          <div className="explain">2019.04 - 2019.07</div>
+          <div className="subtitle">Backend & Frontend Developer (Intern)</div>
+          <div className="desc">ACCESS CO. LTD. (in Taipei)</div>
+          <div className="explain">2018.10 - 2019.02</div>
+          <div className="subtitle">QA Engineer (Intern)</div>
+          <div className="desc">Umbo Computer Vision Inc.</div>
+          <div className="explain">2018.06 - 2019.08</div>
+
+          <div className="title">Education</div>
+          <div className="subtitle">National Taiwan University of Science and Technology (台灣科技大學)</div>
+          <div className="desc">Bachelor of Computer Science and Information Engineering</div>
+          <div className="explain">2015.09 - 2019.08</div>
+          <div className="subtitle">(Japan) Shibaura Institute of Technology</div>
+          <div className="desc">As an exchange student</div>
+          <div className="explain">2019.04 - 2019.08</div>
         </div>
         <div className="col-sm-4">
           <div className="title">Skills</div>
