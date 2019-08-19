59532c6f22e3c380bccedbda8787db2a37dd4271
hiroya8649 <happy8649@gmail.com>
2019-08-19 22:16:59 +0800

    add keep heroku alive func

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	router "github.com/hiroya8649/gomysite/web"
 )
 
+func herokuKeepAlive() {
+	for {
+		time.Sleep(15 * time.Minute)
+		http.Get("http://hiroya-resume.herokuapp.com")
+	}
+}
+
 func main() {
+	go herokuKeepAlive()
 	r := router.SetupRouter()
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
